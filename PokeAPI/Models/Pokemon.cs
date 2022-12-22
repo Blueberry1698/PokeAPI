b4a14b4599bb3786636a93cfd16fdf517049bdd8
Blueberry1698 <lukas.lenzen@web.de>
2022-12-22 12:37:45 +0100

    :heavy_plus_sign: pokemon are having types now

--- PokeAPI/Models/Pokemon.cs
+++ PokeAPI/Models/Pokemon.cs
@@ -4,5 +4,7 @@
     {
         public int PokemonID { get; set; } = 0;
         public string PokemonName { get; set; } = string.Empty;
+        public Type? Type1 { get; set; }
+        public Type? Type2 { get; set; }
     }
 }
